ec99a6923d700a11bd2c6e277b8899359f10c70d
kevinhooke <kevin.hooke@gmail.com>
2022-08-07 18:54:21 -0700

    added Swing invokeLater

--- src/main/java/digitalclock/DigitalClockApp.java
+++ src/main/java/digitalclock/DigitalClockApp.java
@@ -10,9 +10,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
-public class DigitalClockApp extends JFrame{
+public class DigitalClockApp extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
@@ -23,33 +24,38 @@ public class DigitalClockApp extends JFrame{
 
 	private void createFrame() {
 		Canvas canvas = new Canvas();
-		
+
 		this.setBounds(new Rectangle(180, 80));
 		this.add(canvas);
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		Graphics g = canvas.getGraphics();
 		g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 28));
-		
+
 		Timer timer = new Timer();
 		timer.schedule(new TextUpdateTimer(g), 0, 1000);
 	}
 
-	class TextUpdateTimer extends TimerTask{
+	class TextUpdateTimer extends TimerTask {
 
 		private Graphics g;
-		
+
 		public TextUpdateTimer(Graphics g) {
 			this.g = g;
 		}
-		
+
 		@Override
 		public void run() {
-			LocalDateTime date = LocalDateTime.now();
-			g.clearRect(0, 0, 180, 80);
-			g.drawChars(date.format(DateTimeFormatter.ofPattern("HH:mm:ss")).toString().toCharArray(), 0, 8, 20, 24);
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					LocalDateTime date = LocalDateTime.now();
+					g.clearRect(0, 0, 180, 80);
+					g.drawChars(date.format(DateTimeFormatter.ofPattern("HH:mm:ss")).toString().toCharArray(), 0, 8, 20,
+							24);
+				}
+			});
 		}
-		
+
 	}
-	
+
 }
